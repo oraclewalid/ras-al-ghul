e80667ea21fc66c358186a3885d0ecf987227df5
Walid Chergui <walid.chergui@gmail.com>
2021-05-12 00:51:05 +0200

    Fix config and add UT

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,58 @@
 use serde_derive::Deserialize;
-#[derive(Debug, Deserialize)]
-struct ServerConfig {
-    ip: Option<String>,
-    port: Option<u8>,
+
+#[derive(Debug, Deserialize, Clone)]
+struct Config  {
+    server  : Option<ServerConfig>,
+    storage : Option<StorageConfig>,
 }
+
+#[derive(Debug, Deserialize, Clone)]
+struct ServerConfig {
+    bind    : Option<String>,
+    port    : Option<u64>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct StorageConfig {
+    snapshot : Option<bool>,
+    db_file_name: Option<String>,
+    save        : Option<u64>,
+}
+
 
 pub fn get_config() {
     let toml_str = r#"
-    global_string = "test"
-    global_integer = 5
     [server]
     ip = "127.0.0.1"
     port = 80
-    [[peers]]
-    ip = "127.0.0.1"
-    port = 8080
-    [[peers]]
-    ip = "127.0.0.1"
+    [storage]
+    snapshot = true
+    db_file_name = "/tmp/ras/dg.ser"
+    save = 100
 "#;
-
+    
     let decoded: ServerConfig = toml::from_str(toml_str).unwrap();
+}
+
+#[test]
+fn parse_config_in_toml_format() {
+
+    let toml_str = r#"
+        [server]
+        bind = "127.0.0.1"
+        port = 80
+        [storage]
+        snapshot = true
+        db_file_name = "/tmp/ras/ras-al-ghul.db"
+        save = 1000
+    "#;
+    let config: Config= toml::from_str(toml_str).unwrap();
+
+   assert_eq!( config.clone().server.unwrap().bind.unwrap(), "127.0.0.1");
+   assert_eq!( config.clone().server.unwrap().port.unwrap(), 80);
+   assert_eq!( config.clone().storage.unwrap().snapshot.unwrap(), true);
+   assert_eq!( config.clone().storage.unwrap().db_file_name.unwrap(), "/tmp/ras/ras-al-ghul.db");
+   assert_eq!( config.clone().storage.unwrap().save.unwrap(), 1000);
 }
 /*
 use std::fs::File;
@@ -33,4 +66,11 @@ fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
 
     buffer
 }
+tcp-keepalive 300
+
+SNAPSHOTTING  
+save 900 1
+dbfilename dump.rdb
+
+logfile 
 */
