c8167a4d4d1211275a829f065ca23c69692d4717
Walid Chergui <walid.chergui@gmail.com>
2021-11-07 03:26:00 +0100

    Add persistence UT

--- src/database.rs
+++ src/database.rs
@@ -30,9 +30,32 @@ impl  InMemoryDatabase  {
             Err(e) => Ok(()),
         }
     }
+    pub fn load(path: String) -> serde_cbor::Result<InMemoryDatabase> {
+        let db_file = File::open(path);
+        match db_file {
+            Ok(file) => serde_cbor::from_reader(file) ,
+            Err(e) => Ok(InMemoryDatabase::new()),
+        }
+    }
 
-    pub fn new() -> InMemoryDatabase{
+    pub fn new() -> InMemoryDatabase {
         let map :HashMap<String, String> = HashMap::new();
         return InMemoryDatabase{ map : map};
     }
+}
+
+#[test]
+fn persist_db_on_disk() {
+
+
+    let mut db = InMemoryDatabase::new();
+    let path = "/tmp/ras.db".to_string();
+    let key1 = "key1".to_string();
+    db.set("key1".to_string(), "value1".to_string());
+    db.set("key2".to_string(), "value2".to_string());
+
+    db.persist(path.clone());
+    let db2 = InMemoryDatabase::load(path).unwrap();
+    let value1 = db2.get(&key1.clone());
+    assert_eq!( value1, Some(&"value1".to_string()));
 }
